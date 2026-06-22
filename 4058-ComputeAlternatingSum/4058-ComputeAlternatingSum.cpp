cf66405579c1cd0e9bc3fd6ed4dc2c5c22744c47
DEVENDRA PRATAP SINGH <dpsinghrcf@gmail.com>
2026-06-22 12:54:07 +0530

    Create 4058-ComputeAlternatingSum

---
+++ 4058-ComputeAlternatingSum/4058-ComputeAlternatingSum.cpp
@@ -0,0 +1,12 @@
+// Last updated: 6/22/2026, 12:54:50 PM
+class Solution {
+public:
+    int alternatingSum(vector<int>& nums) {
+        int ans = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (i % 2 == 0) ans += nums[i];
+            else ans -= nums[i];
+        }
+        return ans;
+    }
+};

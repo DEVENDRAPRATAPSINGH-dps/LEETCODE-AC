19387867db4a64f6596481ac225aa0d7062dcda3
DEVENDRA PRATAP SINGH <dpsinghrcf@gmail.com>
2026-06-22 12:54:51 +0530

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/242-ValidAnagram.cpp
@@ -0,0 +1,9 @@
+// Last updated: 6/22/2026, 12:55:34 PM
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return s == t;
+    }
+};

37164d4cbdf5bfab08fde93c85e19ff4fd610b3a
DEVENDRA PRATAP SINGH <dpsinghrcf@gmail.com>
2026-06-22 12:55:05 +0530

    Create 66-PlusOne

---
+++ 66-PlusOne/66-PlusOne.cpp
@@ -0,0 +1,15 @@
+// Last updated: 6/22/2026, 12:55:48 PM
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        for (int i = digits.size() - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        digits.insert(digits.begin(), 1);
+        return digits;
+    }
+};

bfd027899ec1dc5a44ad0288711f231866d950ad
DEVENDRA PRATAP SINGH <dpsinghrcf@gmail.com>
2026-07-14 11:43:52 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (99.97%) - LeetSync

--- 4357-number-of-elapsed-seconds-between-two-times/number-of-elapsed-seconds-between-two-times.cpp
+++ 4357-number-of-elapsed-seconds-between-two-times/number-of-elapsed-seconds-between-two-times.cpp
@@ -2,16 +2,15 @@ class Solution {
 public:
     int secondsBetweenTimes(string &shuru, string &khatam) {
 
-        int start =
-            ((shuru[0]-'0')*10 + (shuru[1]-'0'))*3600 +
-            ((shuru[3]-'0')*10 + (shuru[4]-'0'))*60 +
-            ((shuru[6]-'0')*10 + (shuru[7]-'0'));
+        return
+        (((khatam[0]-'0')*10 + khatam[1]-'0') * 3600 +
+         ((khatam[3]-'0')*10 + khatam[4]-'0') * 60 +
+         ((khatam[6]-'0')*10 + khatam[7]-'0'))
 
-        int end =
-            ((khatam[0]-'0')*10 + (khatam[1]-'0'))*3600 +
-            ((khatam[3]-'0')*10 + (khatam[4]-'0'))*60 +
-            ((khatam[6]-'0')*10 + (khatam[7]-'0'));
+        -
 
-        return end - start;
+        (((shuru[0]-'0')*10 + shuru[1]-'0') * 3600 +
+         ((shuru[3]-'0')*10 + shuru[4]-'0') * 60 +
+         ((shuru[6]-'0')*10 + shuru[7]-'0'));
     }
 };

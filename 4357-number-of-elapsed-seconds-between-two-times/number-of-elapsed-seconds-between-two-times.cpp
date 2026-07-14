a95025abfbd1b8ed8f3df3fb00b6cb061a44661c
DEVENDRA PRATAP SINGH <dpsinghrcf@gmail.com>
2026-07-14 11:39:20 +0530

    Time: 1 ms (25.13%) | Memory: 9 MB (84.90%) - LeetSync

--- 4357-number-of-elapsed-seconds-between-two-times/number-of-elapsed-seconds-between-two-times.cpp
+++ 4357-number-of-elapsed-seconds-between-two-times/number-of-elapsed-seconds-between-two-times.cpp
@@ -2,16 +2,15 @@ class Solution {
 public:
     int secondsBetweenTimes(string shuru, string khatam) {
 
-        int h1 = stoi(shuru.substr(0, 2));
-        int m1 = stoi(shuru.substr(3, 2));
-        int s1 = stoi(shuru.substr(6, 2));
+        int start =
+            ((shuru[0] - '0') * 10 + (shuru[1] - '0')) * 3600 +
+            ((shuru[3] - '0') * 10 + (shuru[4] - '0')) * 60 +
+            ((shuru[6] - '0') * 10 + (shuru[7] - '0'));
 
-        int h2 = stoi(khatam.substr(0, 2));
-        int m2 = stoi(khatam.substr(3, 2));
-        int s2 = stoi(khatam.substr(6, 2));
-
-        int start = h1 * 3600 + m1 * 60 + s1;
-        int end = h2 * 3600 + m2 * 60 + s2;
+        int end =
+            ((khatam[0] - '0') * 10 + (khatam[1] - '0')) * 3600 +
+            ((khatam[3] - '0') * 10 + (khatam[4] - '0')) * 60 +
+            ((khatam[6] - '0') * 10 + (khatam[7] - '0'));
 
         return end - start;
     }

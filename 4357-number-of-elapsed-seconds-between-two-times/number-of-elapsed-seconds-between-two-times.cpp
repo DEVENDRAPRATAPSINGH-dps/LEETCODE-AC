58b5163e74497b74ead921b825f3bb45f958b75f
DEVENDRA PRATAP SINGH <dpsinghrcf@gmail.com>
2026-07-14 11:40:32 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (99.99%) - LeetSync

--- 4357-number-of-elapsed-seconds-between-two-times/number-of-elapsed-seconds-between-two-times.cpp
+++ 4357-number-of-elapsed-seconds-between-two-times/number-of-elapsed-seconds-between-two-times.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    int secondsBetweenTimes(string shuru, string khatam) {
+    int secondsBetweenTimes(string &shuru, string &khatam) {
 
         int start =
-            ((shuru[0] - '0') * 10 + (shuru[1] - '0')) * 3600 +
-            ((shuru[3] - '0') * 10 + (shuru[4] - '0')) * 60 +
-            ((shuru[6] - '0') * 10 + (shuru[7] - '0'));
+            ((shuru[0]-'0')*10 + (shuru[1]-'0'))*3600 +
+            ((shuru[3]-'0')*10 + (shuru[4]-'0'))*60 +
+            ((shuru[6]-'0')*10 + (shuru[7]-'0'));
 
         int end =
-            ((khatam[0] - '0') * 10 + (khatam[1] - '0')) * 3600 +
-            ((khatam[3] - '0') * 10 + (khatam[4] - '0')) * 60 +
-            ((khatam[6] - '0') * 10 + (khatam[7] - '0'));
+            ((khatam[0]-'0')*10 + (khatam[1]-'0'))*3600 +
+            ((khatam[3]-'0')*10 + (khatam[4]-'0'))*60 +
+            ((khatam[6]-'0')*10 + (khatam[7]-'0'));
 
         return end - start;
     }
